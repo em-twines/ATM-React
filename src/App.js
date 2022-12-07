d667b2294ff6a40f29962c94e6868bab3988d794
emilytwines <emdelbridge@gmail.com>
2022-12-07 11:08:08 -0500

    created components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+const [username, setUserName] = useState('');
+const [userpw, setUsePw] = useState('');
+const [balance, setBalance] = useState(100); 
+
+
+
   return (
     <div className="App">
-       <h3>Hello World!</h3>
+      <div><NavBar/></div>
+      <div><SetCredentials username = {username} password = {userpw}/></div>
+      <div><ChangeUsernameOrPassword newUsername = {setUserName} newPassword = {setUsePw}/></div>
+      <div><ViewBalance balance = {balance}/></div>
+      <div><DepositFundes balance = {balance} depositBalance = {setBalance}/></div>
+      <div><WithdrawFundes balance = {balance} withdrawBalance = {setBalance}/></div>
+      <div><Logout/></div>
+
     </div>
   );
 }
